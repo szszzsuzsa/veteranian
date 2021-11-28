05cac1ebbd44fcd3ac36c8f4c8cf8b2d5f761cbe
szszzsuzsa <szokeszabo.zsuzsanna@gmail.com>
2021-11-28 19:24:03 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css'
+import './App.css';
+import React, {useState, useEffect} from 'react';
+import Clients from './Components/Clients';
+import LoadingMask from './Components/LoadingMask';
 
 const App = () => {
 
+
+  const [client, setClients] = useState(null);
+  const [showSub, setShowSub] = useState(null);
+  const [response, setResponse] = useState(null);
+
+  const url = "/api/clients?search=${input}";
+
+  const getClients = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setClients(data);
+    } catch (error) {
+      setResponse(false);
+    }
+  };
+
+  const subHandler = () => {
+    setTimeout(function () {
+      setShowSub(true)
+    }, 5000)
+  }
+
+  useEffect(() => {
+    getClients()
+    subHandler()
+  }, [])
+
   return (
     <div className="App">
-      Todo...
+      
+      <div className="App">
+      <h1>Veteranian admin-clients</h1>
+
+      <div><input onClick={()=>setClients(client)} ></input>
+      <button>Get</button></div>
+      {client !== null ? client.map((client, index) => <Clients key={index} client={client} />) :<LoadingMask />}
+   
+    </div>
+    <div>     <Clients></Clients>
+</div>
+
+
+
+
+
     </div>
   )
 }
---
+++ src/Components/Clients.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Clients(index, name) {
+
+    return (
+
+<div>
+<p>Clients</p>
+        <p>{name}</p>
+        <p>{index}</p>
+
+</div>
+
+    )
+    
+}
+
+export default Clients
