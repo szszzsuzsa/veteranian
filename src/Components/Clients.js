ac69959de698bf39004baa93afcdf6ca368d7915
szszzsuzsa <szokeszabo.zsuzsanna@gmail.com>
2021-11-29 17:06:39 +0100

    sixth

--- src/Components/Clients.js
+++ src/Components/Clients.js
@@ -5,17 +5,6 @@ import Pets from "./Pets"
 
 const Client = ({client, pets, key, pet, i}) => {
 
-    
-// let petname = pets.map((pet, i) => pet.name)
-// let petvac = pets.map((pet, i) => pet.isVaccinated)
-// let petanimal = pets.map((pet, i) => pet.animal)
-
-    // const [showDetails, setShowDetails] = useState(false)
-
-    // const setDetails = () => {
-    //     setShowDetails(!showDetails)
-    // }
-   
 
     return (
         <div>
--- src/Components/Pets.js
+++ src/Components/Pets.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react';
 
 const Pets = (pets, props, client, key, pet, name, animal, vac , i, isVaccinatedBo, submitForm) => {
     
-
-
-
     return(
 <div>
 
